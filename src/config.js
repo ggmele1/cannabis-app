85eac2ae172599f57727d781e34c95715b471964
Giuseppe <ggmele1@gmail.com>
2020-10-19 07:30:50 -0700

    git ignore update

--- src/config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-    apiKey: "AIzaSyCU2qKRDnLq9uY0eSLU27UC60dcB_M3Sz8",
-    authDomain: "cannabis-app-43a1d.firebaseapp.com",
-    databaseURL: "https://cannabis-app-43a1d.firebaseio.com",
-  }
-   
-  export default config;
