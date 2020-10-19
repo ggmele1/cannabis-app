90797b58a57f54f6d946634403e2b09c9fb46af0
Giuseppe <ggmele1@gmail.com>
2020-10-19 07:42:16 -0700

    Updates

---
+++ precache-manifest.0815fc27a575a75567afe3ec907e946b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db7fc4b9872bf857a1a18f9e13df72f5",
+    "url": "/cannabis-app/index.html"
+  },
+  {
+    "revision": "5337e7360a350c206058",
+    "url": "/cannabis-app/static/css/main.e78dc6f6.chunk.css"
+  },
+  {
+    "revision": "299170ac3b1985873c32",
+    "url": "/cannabis-app/static/js/2.aef34e0d.chunk.js"
+  },
+  {
+    "revision": "f6bb6e7e06135325042ea16538284523",
+    "url": "/cannabis-app/static/js/2.aef34e0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5337e7360a350c206058",
+    "url": "/cannabis-app/static/js/main.7d4d1373.chunk.js"
+  },
+  {
+    "revision": "76e1d428254732a1ff48",
+    "url": "/cannabis-app/static/js/runtime-main.85d3dfeb.js"
+  },
+  {
+    "revision": "d7636b3e2832aaab586e78ce77f2e470",
+    "url": "/cannabis-app/static/media/DopestData.d7636b3e.png"
+  },
+  {
+    "revision": "18a0a23e719b1f6e0aad17ab7a49dfc5",
+    "url": "/cannabis-app/static/media/image.18a0a23e.png"
+  }
+]);
